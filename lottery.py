487ce4488cec69604066838b1e286358f7da8a4f
echo "rilschib <119910880+rilschib@users.noreply.github.com>
2025-01-30 08:55:21 -0600

    gamble
    gamble

--- lottery.py
+++ lottery.py
@@ -1,20 +1,24 @@
 import random
 import time
-from player_class import *
-from randomber import *
+class Player:
+    def __init__(wallet, name):
+        self.balance = balance
+        self.name = name
+win = 10
+bigwin = 100
+biggerwin = 1000
+grandprize = 10000
+icons = ["A", "@", "^", "%", "~"]
+machine = "_______\n|0 0 0|\n|-=*=-|\n|_____|"
 print("What name should we put this under?")
 name = input()
-wallet = float(random.randrange(100, 10000, 1))
-while wallet > 9:
-    print(name, " has $",wallet," in their wallet. don't ask how we know this.")
-    print("winning prize is $", randomber.prizemoney, ", pick a number between 1 and 25, and if your number matches the random number, you win the prize money.")
-    time.sleep(2)
-    input("pick your number: ")
-    if input == winnumber:
-        print(f"you won! your prize is $",prizemoney)
-        if input == winnumber:
-             wallet += prizemoney
-    elif input is not winnumber:
-        print("you lost. you are out by $10")
-        wallet -= 10
-        time.sleep(2)
+balance = float(random.randrange(1000, 10000, 10))
+while True:
+    print(name, " your balance is $",balance,"and the grand prize is", grandprize, ". to roll, type roll.")
+    userin = input().strip().lower()
+    if userin == "roll":
+        print("roll the slots!")
+        print(machine)
+        random.choice(icons)
+else:
+    print("dumbo")
