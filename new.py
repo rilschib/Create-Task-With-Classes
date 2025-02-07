287183e45751052a102a94f33038ebe1ff0e5a03
echo "rilschib <119910880+rilschib@users.noreply.github.com>
2025-02-07 08:56:27 -0600

    Add files via upload

---
+++ new.py
@@ -0,0 +1,28 @@
+import random
+import time
+class Player:
+    def __init__(wallet, name):
+        self.balance = balance
+        self.name = name
+fee = 10
+win = 100
+grandprize = 777
+print("What name should we put this under?")
+name = input()
+balance = float(random.randrange(1000, 10000, 10))
+while True:
+    print(name, " your balance is $",balance,"and the grand prize is", grandprize, ". pick a number between 1 and 10.")
+    userin = input().strip().lower()
+    if userin in range(1,11,1):
+        balance -= float(fee)
+        print("roll the slots!")
+        print(machine)
+        if machine == lsgrand:
+            print(f"Grand prize of $",grandprize," was won!")
+            balance ,= float(grandprize)
+            if machine == random.randrange(1,11,1):
+                print(f"win get! won $", win, "!")
+        if userin not in range(1,11,1):
+            print("pick valid number.")
+else:
+    print("lsoer")
